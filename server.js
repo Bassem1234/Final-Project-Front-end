ebebe3bcb8835efc6567303355a7a104be25d097
Bassem1234 <bassem.rbaia1@gmail.com>
2022-06-20 14:05:24 +0200

    js

--- server.js
+++ server.js
@@ -5,11 +5,11 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static('Angular Template'));
+app.use(express.static('./dist/Angular Template'));
 
 app.get('/*', (req, res) =>
-    res.sendFile('index.html', {root: 'Angular Template/'}),
+    res.sendFile('index.html', {root: 'dist/Angular Template/'}),
 );
 
 // Start the app by listening on the default Heroku port
-app.listen(process.env.PORT || 8080);
+app.listen(8080);
