fbb6ed8c3942ac3123e1893cd6161fdae929175a
Bassem1234 <bassem.rbaia1@gmail.com>
2022-06-20 18:06:12 +0200

    Ovent

--- server.js
+++ server.js
@@ -6,13 +6,13 @@ const app = express();
 
 // Serve only the static files form the dist directory
 //app.use(express.static('./dist/ovent'));
-app.use(express.static(__dirname + '/dist/ovent'));
+app.use(express.static(__dirname + '/dist/Ovent'));
 
 // app.get('/*', (req, res) =>
 //     res.sendFile('index.html', {root: 'dist/ovent/'}),
 // );
 app.get('/*', (req, res) =>
-    res.sendFile(path.join(__dirname + 'dist/ovent/index.html')),
+    res.sendFile(path.join(__dirname + 'dist/Ovent/index.html')),
 );
 
 // Start the app by listening on the default Heroku port
