bcffe273473b09843b7e1c88c0482003b4f0ce71
Bassem1234 <bassem.rbaia1@gmail.com>
2022-06-21 14:22:02 +0200

    port

--- src/app/managment-service.service.ts
+++ src/app/managment-service.service.ts
@@ -9,7 +9,7 @@ import { Router } from '@angular/router';
 })
 export class ManagmentServiceService {
 //baseURL: string = environment.baseURL;
-baseURL = 'https://tseker-back.herokuapp.com/';
+baseURL = 'https://tseker-back.herokuapp.com:4000';
   constructor(private http: HttpClient, private router: Router) { }
 
   login(userData: any){
