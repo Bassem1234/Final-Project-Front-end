930e94b909ed79540c119524b4b73f948767e75a
Bassem1234 <bassem.rbaia1@gmail.com>
2022-06-22 17:07:26 +0200

    kjb

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -26,7 +26,7 @@ export class AppComponent implements OnInit {
     window.scroll(0,0);
   }
   private createTitle(routesCollection: Breadcrumb[]) {
-    const title = 'Ovent Angular';
+    const title = 'Tseker';
     const titles = routesCollection.filter((route) => route.displayName);
 
     if (!titles.length) { return title; }
