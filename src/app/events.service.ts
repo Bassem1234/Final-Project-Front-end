211daaef2836e5916b970e092e3f62a1e9ae368d
Bassem1234 <bassem.rbaia1@gmail.com>
2022-06-22 14:50:49 +0200

    http

--- src/app/events.service.ts
+++ src/app/events.service.ts
@@ -8,7 +8,7 @@ import { environment } from 'src/environments/environment';
 export class EventsService {
   //baseURL: string = environment.baseURL;
   //baseURL = 'http://localhost:4000';
-  baseURL = 'https://tseker-back.herokuapp.com';
+  baseURL = 'http://tseker-back.herokuapp.com';
   constructor(private http: HttpClient) { }
   getEvents(){  
     return this.http.get(`${this.baseURL}/api/events/`);
